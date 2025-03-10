1c5cfd1838c0e0fa94c372e721853bd661328413
chadish <chadish7@gmail.com>
2025-03-10 15:18:10 +0000

    update day1

--- typescript/2024/day1/day1.ts
+++ typescript/2024/day1/day1.ts
@@ -15,13 +15,14 @@ const numbers = lines.map((line) => {
     }
     return 0;
 });
+
 columnOne = columnOne.sort((a, b) => a - b)
 columnTwo = columnTwo.sort((a, b) => a - b)
 var diff_sum: number = 0;
 
 for (let i = 0; i < columnOne.length; i++) {
     const difference = columnOne[i] - columnTwo[i];
-    console.log(difference);
+    // console.log(difference);
     if (difference < 0) {
         diff_sum += difference * -1;;
     } else {
@@ -34,5 +35,11 @@ console.log("Difference score",diff_sum);
 var sim_sum:number = 0;
 
 columnOne.forEach(element => {
-    
+    columnTwo.filter((value) => {
+        if (element == value) {
+            sim_sum += element;
+        }
+    })
 })
+
+console.log("Similarity score", sim_sum);
